ab266b5ea24d26a2889ba5532778b724f86359db
0xkid-root <prohackers698@gmail.com>
2025-08-22 02:26:02 +0530

    web3auth setup not intergation update readme.md file,

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,29 @@ import './index.css';
 import App from './App';
 import { store } from './store/store';
 import {MetaMaskProvider} from './providers/MetaMaskProvider';
+import web3AuthContextConfig from './providers/web3authContext';
+
+// Web3Auth and Wagmi imports
+import { Web3AuthProvider } from "@web3auth/modal/react";
+import { WagmiProvider } from "@web3auth/modal/react/wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <MetaMaskProvider>
+      <Web3AuthProvider config={web3AuthContextConfig}>
+        <QueryClientProvider client={queryClient}>
+          <WagmiProvider>
+      {/* <MetaMaskProvider> */}
         <App />
-      </MetaMaskProvider>
+      {/* </MetaMaskProvider> */}
+      </WagmiProvider>
+      </QueryClientProvider>
+
+      </Web3AuthProvider>
     </Provider>
+
   </StrictMode>
 );
--- src/pages/SPV/AddSpv/index.tsx
+++ src/pages/SPV/AddSpv/index.tsx
@@ -427,7 +427,7 @@ function Index() {
           </div>
         </motion.div>
       </div>
-    </div>
+    </FormProvider>
   );
 }
 
